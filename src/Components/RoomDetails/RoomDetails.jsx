b9410f2a1c3b25d07fe94588a210673abb70084a
Ronit Cohen <ron13795@gmail.com>
2022-10-10 23:49:00 +0300

    t

--- src/Components/RoomDetails/RoomDetails.jsx
+++ src/Components/RoomDetails/RoomDetails.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import context from "../../context";
 import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import AddProduct from "../AddProduct/AddProduct";
 import Product from "../Product/Product";
 
 export default function RoomDetails() {
+  const nav = useNavigate();
   const [showAddComponent, setShowAddComponent] = useState(false);
   const { selectedRoom, products, setProducts } = useContext(context);
+  const productToShow = products[selectedRoom.id]
+    ? products[selectedRoom.id]
+    : [];
   return (
     <div>
       <h2>name : {selectedRoom.name}</h2>
       <br />
       <h2>type : {selectedRoom.type}</h2> <br />
-      {products[selectedRoom.id].map((product, index) => {
+      {productToShow.map((product, index) => {
         return (
           <Product
             key={index}
@@ -28,6 +33,13 @@ export default function RoomDetails() {
         }}
       >
         add product
+      </button>
+      <button
+        onClick={() => {
+          nav("/");
+        }}
+      >
+        back
       </button>
       {showAddComponent && (
         <AddProduct setShowAddComponent={setShowAddComponent} />
